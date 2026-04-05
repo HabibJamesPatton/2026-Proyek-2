8aab27a529186c8d3432790aee6509f4c5ec8824
ABLEH25 <fawwazfaleh65@gmail.com>
2026-04-05 23:38:12 +0700

    fix: perbaikan kesalahan penulisan

--- faleh.h
+++ faleh.h
@@ -3,19 +3,19 @@
 
 //Deklarasi Fungsi
 
-/*new: Mengosongkan layar editor*/
-void newFile();
+// --Fitur New File--
+void newFile(); //-- Membuat file baru, memulai dengan buffer kosong, dan mengatur nama file global menjadi string kosong--
 
-/*open: Mengambil data dari hardisk ke memori(RAM) agar bisa diedit*/
-void openFile(char *filename)
+// -- Fitur open file --
+void openFile(char *filename); //-- Mengambil data dari hardisk ke memori(RAM) agar bisa diedit--
 
-/*saveAs: Membuat file baru atau menimpa file lama dengan nama spesifik, memindahkan data dari layar ke disk*/
-void saveAs(char *filename, char *content);
+// -- Fitur Save As --
+void saveAs(char *filename, char *content); //--Membuat file baru atau menimpa file lama dengan nama spesifik, memindahkan data dari layar ke disk--
 
-/*save: Memindahkan data dari layar ke Disk*/
-void save(char *content);
+// -- Fitur Save --
+void save(char *content); /*save: Memindahkan data dari layar ke Disk*/
 
-/*Memutus koneksi antara program dan file*/
-void closeFile();
+// -- Fitur Close File --
+void closeFile(); // -- Memutus koneksi antara program dan file --
 
 #endif
